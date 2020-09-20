1113f1134f9c23f4f92ab8eb347e98d17aac34d0
Leon Richardt <leon.richardt@gmail.com>
2020-09-20 17:45:54 +0200

    feat: Add IPC support for stopping and (re)starting

---
+++ gui/src/IpcServer.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "StopwatchWidget.hpp"
+
+#include <QLocalServer>
+
+namespace stopwatch {
+
+enum class Message : char { STOP = 0, START = 1 };
+enum class Response : char { OK = 0, ERROR = 1 };
+
+class IpcServer : public QObject
+{
+    Q_OBJECT
+
+public:
+    static constexpr char const *SERVER_NAME = "stopwatch11";
+
+    IpcServer(StopwatchWidget *stopwatch, QObject *parent = nullptr);
+    ~IpcServer();
+
+    /**
+     * @brief
+     * Return `true` if the server could be started successfully;
+     * `false` otherwise.
+     */
+    bool start();
+
+    void stop();
+
+private:
+    void handleConnection();
+
+    StopwatchWidget *stopwatch_{};
+    QLocalServer *server_{};
+};
+
+}  // namespace stopwatch
